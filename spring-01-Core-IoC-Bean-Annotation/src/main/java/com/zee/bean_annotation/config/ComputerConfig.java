50ba32cdc89d54a1e8d3a0d55de4c3c5f854d8dc
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-18 14:25:31 -0400

    IoC - Bean Annotation

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/zee/bean_annotation/config/ComputerConfig.java
@@ -0,0 +1,51 @@
+package com.zee.bean_annotation.config;
+
+import com.zee.bean_annotation.casefactory.Case;
+import com.zee.bean_annotation.casefactory.DellCase;
+import com.zee.bean_annotation.monitorfactory.AcerMonitor;
+import com.zee.bean_annotation.monitorfactory.Monitor;
+import com.zee.bean_annotation.monitorfactory.SonyMonitor;
+import com.zee.bean_annotation.motherboardfactory.AsusMotherboard;
+import com.zee.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ComputerConfig {
+
+    @Bean(name="sony")
+    public Monitor monitorSony(){
+
+        return new SonyMonitor("25 inch Beast","Sony",25);
+    }
+
+    @Bean(name="sony2")
+    public Monitor monitorSony2(){
+
+        return new SonyMonitor("40 inch Beast","Sony",40);
+    }
+
+
+    @Bean
+    @Primary
+    public Monitor monitorAcer(){
+        return new AcerMonitor("23 inch Beast","Acer",23);
+    }
+
+
+    @Bean
+    public Case caseDell(){
+        return new DellCase("220B","Dell","240");
+    }
+
+    @Bean
+    public Motherboard motherboardAsus(){
+        return new AsusMotherboard("BJ-200","Asus",4,6,"v2.44");
+    }
+
+
+
+
+}
+
