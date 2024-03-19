2505351b248bdb722bb6dafe68186da63bc02ad1
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-19 19:15:00 -0400

    Wiring

---
+++ spring-03-Wiring/src/main/java/com/zee/ComputerTest.java
@@ -0,0 +1,20 @@
+package com.zee;
+
+import com.zee.casefactory.Case;
+import com.zee.config.PcConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(PcConfig.class);
+
+        Case theCase = container.getBean(Case.class);
+
+        System.out.println(theCase.getDimensions().getDepth());
+    }
+}
+
