707f4ccdf6e5242faa769ee790374fc8ae394e19
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-19 18:16:08 -0400

    Stereotype Annotation

---
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/zee/stereotype_annotation/PC.java
@@ -0,0 +1,33 @@
+package com.zee.stereotype_annotation;
+
+
+import com.zee.stereotype_annotation.casefactory.Case;
+import com.zee.stereotype_annotation.monitorfactory.Monitor;
+import com.zee.stereotype_annotation.motherboardfactory.Motherboard;
+import lombok.Getter;
+
+@Getter
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+        // Fancy graphics
+        monitor.drawPixelAt();
+    }
+
+}
+
