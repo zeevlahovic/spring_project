114cb31d156bb8b069a0a3f2837387c3aee3b0ef
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-21 19:18:13 -0400

    Spring Boot

--- spring-07-SpringBoot-Demo/src/main/java/com/zee/spring07springbootdemo/ZeeApplication.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/zee/spring07springbootdemo/ZeeApplication.java
@@ -1,5 +1,7 @@
 package com.zee.spring07springbootdemo;
 
+import com.zee.spring07springbootdemo.model.Comment;
+import com.zee.spring07springbootdemo.service.CommentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,7 +11,14 @@ public class ZeeApplication {
 
     public static void main(String[] args) {
 
+        Comment comment  = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
         ApplicationContext container = SpringApplication.run(ZeeApplication.class, args);
+
+        CommentService commentService = container.getBean(CommentService.class);
+        commentService.publishComment(comment);
     }
 
 
