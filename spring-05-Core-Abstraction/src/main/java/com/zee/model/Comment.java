5c5c5bc6521fd451da5ddf74bbd0d06291563ead
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-21 11:53:56 -0400

    Spring Boot

--- spring-05-Core-Abstraction/src/main/java/com/zee/model/Comment.java
+++ spring-05-Core-Abstraction/src/main/java/com/zee/model/Comment.java
@@ -2,8 +2,10 @@ package com.zee.model;
 
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class Comment {
     private String author;
     private String text;
