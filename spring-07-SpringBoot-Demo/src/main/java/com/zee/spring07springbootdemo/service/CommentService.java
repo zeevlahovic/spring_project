cc02ad5e109955588a94fd4a3fef832a25c94e03
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-21 19:30:45 -0400

    Spring Boot

--- spring-07-SpringBoot-Demo/src/main/java/com/zee/spring07springbootdemo/service/CommentService.java
+++ spring-07-SpringBoot-Demo/src/main/java/com/zee/spring07springbootdemo/service/CommentService.java
@@ -1,6 +1,8 @@
 package com.zee.spring07springbootdemo.service;
 
 
+import com.zee.spring07springbootdemo.config.AppConfigData;
+import com.zee.spring07springbootdemo.config.DBConfigData;
 import com.zee.spring07springbootdemo.model.Comment;
 import com.zee.spring07springbootdemo.proxy.CommentNotificationProxy;
 import com.zee.spring07springbootdemo.repository.CommentRepository;
@@ -10,21 +12,35 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Scope("prototype")
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
+    private final AppConfigData appConfigData;
+    private final DBConfigData dbConfigData;
 
-    public CommentService(CommentRepository commentRepository,@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy, AppConfigData appConfigData, DBConfigData dbConfigData) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        this.appConfigData = appConfigData;
+        this.dbConfigData = dbConfigData;
     }
 
     public void publishComment(Comment comment){
         commentRepository.storeComment(comment);
         commentNotificationProxy.sendComment(comment);
     }
-}
 
+    public void printConfigData() {
+        System.out.println(appConfigData.getUserName());
+        System.out.println(appConfigData.getPassword());
+        System.out.println(appConfigData.getUrl());
+
+    }
+
+    public void printDbConfigData(){
+        System.out.println(dbConfigData.getUsername());
+        System.out.println(dbConfigData.getPassword());
+        System.out.println(dbConfigData.getType());
+    }
+}
