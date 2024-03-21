7eaa62d52e0d54a9237f192c6a40b8fe83997837
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-21 13:22:44 -0400

    Spring Boot

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/zee/spring07springbootdemo/ZeeApplication.java
@@ -0,0 +1,14 @@
+package com.zee.spring07springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeeApplication {
+
+    public static void main(String[] args) {  //main
+        SpringApplication.run(ZeeApplication.class, args);
+    }
+
+
+}
---
+++ spring-07-SpringBoot-Demo/src/test/java/com/zee/spring07springbootdemo/ZeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee.spring07springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
