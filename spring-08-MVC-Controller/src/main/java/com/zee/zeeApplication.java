fb13e77825f82a3e2703b788cafced97a327c168
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-25 11:22:50 -0400

    spring MVC

---
+++ spring-08-MVC-Controller/src/main/java/com/zee/zeeApplication.java
@@ -0,0 +1,15 @@
+package com.zee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class zeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(zeeApplication.class, args);
+
+		System.out.println("java");
+	}
+
+}
