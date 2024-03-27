a05311dfc0bd896b728aaf9e4c97f5f4711bfefe
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-27 10:42:08 -0400

    ThymeleafApplication

---
+++ spring-10-Thymeleaf/src/main/java/com/zee/controller/StudentController.java
@@ -0,0 +1,4 @@
+package com.zee.controller;
+
+public class StudentController {
+}
---
+++ spring-10-Thymeleaf/src/test/java/com/zee/Spring10ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
