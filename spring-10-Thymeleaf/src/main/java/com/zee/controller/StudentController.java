7ad42a1588aa6dfcad8e2d3a50462f6cc54f959d
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-03-27 11:06:49 -0400

    ThymeleafApplication

--- spring-10-Thymeleaf/src/main/java/com/zee/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/zee/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.zee.controller;
 
+import com.zee.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
 public class StudentController {
+
+    @RequestMapping("/register") //localhost:8080/student/register
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+
+    @RequestMapping("/welcome") //localhost:8080/student/welcome?name=Ozzy
+    public String welcome(){
+
+
+        return "student/welcome";
+    }
 }
