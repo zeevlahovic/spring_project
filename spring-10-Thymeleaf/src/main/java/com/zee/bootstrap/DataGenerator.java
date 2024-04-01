807200f17df5aa64d4f3670df3bf181ef87e6c55
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-01 11:31:31 -0400

    ThymeleafApplication

--- spring-10-Thymeleaf/src/main/java/com/zee/bootstrap/DataGenerator.java
+++ spring-10-Thymeleaf/src/main/java/com/zee/bootstrap/DataGenerator.java
@@ -1,5 +1,6 @@
 package com.zee.bootstrap;
 
+
 import com.github.javafaker.Faker;
 import com.zee.model.Student;
 
@@ -11,6 +12,7 @@ public class DataGenerator {
     public static List<Student> createStudent(){
 
         List<Student> students = Arrays.asList(
+                new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
                 new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
                 new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
                 new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
--- spring-10-Thymeleaf/src/main/java/com/zee/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/zee/controller/StudentController.java
@@ -3,24 +3,27 @@ package com.zee.controller;
 import com.zee.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register") //localhost:8080/student/register
+    //    @RequestMapping(value = "/register",method = RequestMethod.GET) //localhost:8080/student/register
+    @GetMapping("/register")
     public String register(Model model){
 
         model.addAttribute("students", DataGenerator.createStudent());
 
         return "student/register";
+
     }
 
-    @RequestMapping("/welcome") //localhost:8080/student/welcome?name=Ozzy
-    public String welcome(){
-
+    @RequestMapping(value = "/welcome",method = RequestMethod.POST) //localhost:8080/student/welcome?name=Ozzy
+    @PostMapping("/welcome")
+    public String welcome(@RequestParam String name){
 
         return "student/welcome";
     }
+
 }
