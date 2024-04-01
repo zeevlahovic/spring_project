e69974453a60493752856af1cf27cea0b73b50b1
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-01 11:53:41 -0400

    ThymeleafApplication

--- spring-10-Thymeleaf/src/main/java/com/zee/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/zee/controller/MentorController.java
@@ -1,22 +1,40 @@
 package com.zee.controller;
 
+import com.zee.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-    @RequestMapping("/register") //localhost:8080/mentor/register
-    public String register(){
+    @GetMapping("/register") //localhost:8080/mentor/register
+    public String register(Model model){
 
-        return "student/register";
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
+
+        model.addAttribute("mentor",new Mentor());
+
+        return "mentor/mentor-register";
     }
 
-    @RequestMapping("/drop") //localhost:8080/mentor/drop
-    public String drop(){
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
 
-        return "student/register";
+//        return "mentor/mentor-register";
+        return "redirect:/mentor/register";
     }
+
+
+
 
 }
+
