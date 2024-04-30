1fa05581fe7c776f17bd4ca89ac91efd52c9d435
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-04-29 20:46:21 -0400

    DataProject

---
+++ spring-11-Data-TableCreation/src/main/java/com/zee/sprin11datatablecreation/DataApplication.java
@@ -0,0 +1,17 @@
+package com.zee.sprin11datatablecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataApplication.class, args);
+    }
+
+//test33434
+
+
+    }
+
---
+++ spring-11-Data-TableCreation/src/test/java/com/zee/sprin11datatablecreation/Sprin11DataTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zee.sprin11datatablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprin11DataTableCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
