127b3371d547d964bb452b01b4ed4399068bd5a4
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-05-01 10:27:02 -0400

    DataRelationship

--- spring-12-Data-Relationship/src/main/java/com/zee/entity/PaymentDetail.java
+++ spring-12-Data-Relationship/src/main/java/com/zee/entity/PaymentDetail.java
@@ -24,6 +24,9 @@ public class PaymentDetail {
     @Column(columnDefinition =  "DATE")
     private LocalDate payoutDate;
 
+    @OneToOne(mappedBy = "paymentDetail")
+    private Payment payment;
+
 
 
     public PaymentDetail(BigDecimal merchantPayoutAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
