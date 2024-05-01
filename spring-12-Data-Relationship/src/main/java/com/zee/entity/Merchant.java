60da08f4476e93fe16881ee80701b5260b708102
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-05-01 11:40:44 -0400

    DataRelationship

---
+++ spring-12-Data-Relationship/src/main/java/com/zee/entity/Merchant.java
@@ -0,0 +1,39 @@
+package com.zee.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "merchants")
+@NoArgsConstructor
+@Data
+
+public class Merchant {
+
+    @Id
+    private Long id;
+
+    private String name;
+    private String code;
+    private BigDecimal transactionFee;
+    private BigDecimal commissionRate;
+    private Integer payoutDelayCount;
+
+    @OneToMany
+    private List<Payment>paymentList;
+
+    public Merchant(String name, String code, BigDecimal transactionFee, BigDecimal commissionRate, Integer payoutDelayCount) {
+        this.name = name;
+        this.code = code;
+        this.transactionFee = transactionFee;
+        this.commissionRate = commissionRate;
+        this.payoutDelayCount = payoutDelayCount;
+    }
+}
