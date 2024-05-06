9639f0c7f38575dd17a72c2462b1c6cbf9d8a699
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-05-06 18:49:34 -0400

    CinemaLabApplication

--- spring-13-Data-CinemaLab/src/main/java/com/zee/CinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/zee/CinemaLabApplication.java
@@ -1,13 +1,23 @@
 package com.zee;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CinemaLabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CinemaLabApplication.class, args);
+    }
+
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
     }
 
 }
