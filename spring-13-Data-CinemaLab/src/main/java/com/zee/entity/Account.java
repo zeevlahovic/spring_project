c48e71716a7f5b1e60c99375245676ef97fa4a79
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-05-06 18:33:46 -0400

    CinemaLabApplication

---
+++ spring-13-Data-CinemaLab/src/main/java/com/zee/entity/Account.java
@@ -0,0 +1,25 @@
+package com.zee.entity;
+
+import com.zee.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "Account_details")
+public class Account extends BaseEntity {
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+    @OneToOne(mappedBy = "account")
+    private User user;
+}
---
+++ spring-13-Data-CinemaLab/src/main/java/com/zee/entity/User.java
@@ -0,0 +1,21 @@
+package com.zee.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user_account")
+public class User extends BaseEntity{
+
+    private String email;
+    private String username;
+    private String password;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_details_id")
+    private Account account;
+
+}
