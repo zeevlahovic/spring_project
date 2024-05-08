788bea41eea02ed9c44906c030e0589fa1eff3de
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-05-08 13:23:40 -0400

    Queries

--- spring-14-Data-Queries/src/main/java/com/zee/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/zee/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.zee.repository;
 
 import com.zee.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -25,4 +27,8 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 
     //Find all courses by category and returns a stream
     Stream<Course> streamAllByCategory(String category);
+
+    @Query("SELECT c FROM Course c WHERE c.category = :category AND c.rating > :rating")
+    List<Course> retrieveAllByCategoryAndRatingGreaterThan(@Param("category") String category, @Param("rating") int rating);
+
 }
