c1eb3dcd15021a594eced2dff5c7caf842eef60e
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-05-08 10:49:04 -0400

    Queries

--- spring-14-Data-Queries/src/main/java/com/zee/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/zee/repository/CourseRepository.java
@@ -4,6 +4,7 @@ import com.zee.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
@@ -12,4 +13,16 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 
     //Find all courses by category and order the entities by name
     List<Course> findByCategoryOrderByName(String category);
+
+    //Checks if a course with the supplied name exists. Return true if exists, false otherwise
+    boolean existsByName(String name);
+
+    //Returns the count of courses for the given category
+    int countByCategory(String category);
+
+    //Finds all the courses that start with the given course name string
+    List<Course> findByNameStartingWith(String pattern);
+
+    //Find all courses by category and returns a stream
+    Stream<Course> streamAllByCategory(String category);
 }
