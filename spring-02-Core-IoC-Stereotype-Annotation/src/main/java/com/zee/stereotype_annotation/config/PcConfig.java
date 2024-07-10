352f70225fe38b1e499e54be0e7b0e467cc99216
Zee Vlahovic <zikavlahovic@yahoo.com>
2024-07-09 20:43:08 -0400

    REST

--- spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/zee/stereotype_annotation/config/PcConfig.java
+++ spring-02-Core-IoC-Stereotype-Annotation/src/main/java/com/zee/stereotype_annotation/config/PcConfig.java
@@ -3,6 +3,7 @@ package com.zee.stereotype_annotation.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @ComponentScan("com.zee.stereotype_annotation")
 public class PcConfig {
